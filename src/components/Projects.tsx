8db065101fddd33afb5023c1940f1050b304e1f0
Nick Otimo <160436505+nick-otimo@users.noreply.github.com>
2025-05-14 14:43:27 +0300

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,7 +3,16 @@ import { Github, Eye, ExternalLink } from 'lucide-react';
 
 const projects = [
   {
-    id: 1,
+  id: 1,
+  title: "Extrimity Water Tank",
+  description: "Water Tank design.",
+  image: "/images/WATERTANK2.png", //local path
+  technologies: ["Autodesk Inventor"],
+  comingSoon: true
+}
+
+  {
+    id: 2,
     title: "Project Hostara",
     description: "Coming soon: A desktop application focusing on user interface design and seamless functionality implementation.",
     image: "https://scontent.fnbo8-1.fna.fbcdn.net/v/t39.30808-6/496526337_659673323719790_8994599267912308178_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_ohc=25McYtRQQY4Q7kNvwEc6kjR&_nc_oc=AdknDpVr2aYMLU_P6Ji2CVa-Z-9QivWOpl_J856PYc2nAmr5pfN390eW4ZS0pzfPEcg&_nc_zt=23&_nc_ht=scontent.fnbo8-1.fna&_nc_gid=tcSaiq-nvwSSIcqOmU_k-A&oh=00_AfJ-1AEjd3TPFYNzTXs6StOo3_Ysz552_J8mSnhxkCb2Ww&oe=6824DA80",
@@ -11,7 +20,7 @@ const projects = [
     comingSoon: true
   },
   {
-    id: 2,
+    id: 3,
     title: "AgroXTREME Website Development",
     description: "Coming soon: The official AgroXTREME Limited website to showcase agricultural innovations and services.",
     image: "https://scontent.fnbo8-1.fna.fbcdn.net/v/t39.30808-6/496077000_659669833720139_1784315667198857297_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=VbmCnlEaDkMQ7kNvwFGGc36&_nc_oc=Adn_llhwivlsNXvF5JOGgCg32bR4AV01BaKPFSRlBsiLZKNKB3RsI4yGsTyVl8MOC4U&_nc_zt=23&_nc_ht=scontent.fnbo8-1.fna&_nc_gid=RZYeGjseSmefdTyXs8aG1g&oh=00_AfKykJXYGBE55qhPU9T5qExuc5GJbtFBAF-M5kQUDzy9Zw&oe=6824E138",
